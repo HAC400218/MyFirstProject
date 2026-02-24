689f8e41595ddf54320140968c2e57ac629a9345
harish <harish.bhardwaj@capgemini.com>
2026-02-24 14:49:22 +0530

    update handler method in light controller class

--- Traffic/src/main/java/com/trafficlight/sys/controller/TrafficLightController.java
+++ Traffic/src/main/java/com/trafficlight/sys/controller/TrafficLightController.java
@@ -1,9 +1,12 @@
 package com.trafficlight.sys.controller;
 
+import com.trafficlight.sys.model.TrafficDirection;
+import com.trafficlight.sys.service.InterSection;
 import com.trafficlight.sys.service.TrafficLightService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("intersections/api")
@@ -11,4 +14,35 @@ public class TrafficLightController {
 
     @Autowired
     private TrafficLightService trafficLightService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> createInterSections(@PathVariable("id") String intersectionId){
+        InterSection interSections=trafficLightService.TrafficLightService(intersectionId);
+        return new ResponseEntity<>(interSections, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}/green/{TrafficDirection}")
+    public ResponseEntity<?> changeTrafficColorGreen(@PathVariable("id") String intersectionId,
+                     @PathVariable("TrafficDirection") TrafficDirection direction){
+        trafficLightService.changeLightColor(intersectionId,direction);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}/pause")
+    public ResponseEntity<?> changePause(@PathVariable("id") String intersectionId){
+        trafficLightService.changePause(intersectionId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}/resume")
+    public ResponseEntity<?> changeResume(@PathVariable("id") String intersectionId){
+        trafficLightService.changeResume(intersectionId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("{id}/currentstate") 
+    public ResponseEntity<?> getCurrentState(@PathVariable("id") String intersectionId){
+        InterSection interSection=trafficLightService.getIntersections(intersectionId);
+        return new ResponseEntity<>(interSection,HttpStatus.OK);
+    }
 }
