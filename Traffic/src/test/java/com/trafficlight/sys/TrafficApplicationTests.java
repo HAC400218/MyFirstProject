9f26dd99f993bd12697e6ea892a78c8f623c5744
harish <harish.bhardwaj@capgemini.com>
2026-02-24 12:02:01 +0530

    'myproject_structure_code_commit'

---
+++ Traffic/src/test/java/com/trafficlight/sys/TrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafficlight.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
